86ca257b51964ba34474a4ca9522b9dc6c144d15
Gecko93 <nicolas.lpfan@gmail.com>
2024-06-21 23:08:57 -0300

    primer commit

---
+++ app/Views/front/login.php
@@ -0,0 +1,17 @@
+<form>
+  <div class="form-group">
+    <label for="exampleInputEmail1">Correo Electrónico</label>
+    <input type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Correo">
+    <small id="emailHelp" class="form-text text-muted">Nunca compartiremos su correo con nadie más.</small>
+  </div>
+  <div class="form-group">
+    <label for="exampleInputPassword1">Contraseña</label>
+    <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Contraseña">
+  </div>
+  <div class="form-check">
+    <input type="checkbox" class="form-check-input" id="exampleCheck1">
+    <label class="form-check-label" for="exampleCheck1">Recordarme</label>
+  </div>
+  <button type="submit" value="Inresar" class="btn btn-success">Ingresar</button>
+  <button type="submit" value="Inresar" class="btn btn-danger">Cancelar</button>
+</form>
---
+++ app/Views/front/registro.php
@@ -0,0 +1,53 @@
+
+
+<form>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputEmail4">Correo Electrónico</label>
+      <input type="email" class="form-control" id="inputEmail4" placeholder="Correo@algo.com">
+    </div>
+    <div class="form-group col-md-6">
+      <label for="inputPassword4">Contraseña</label>
+      <input type="password" class="form-control" id="inputPassword4" placeholder="Contraseña">
+    </div>
+  </div>
+  <div class="form-group">
+    <label for="inputAddress">Dirección</label>
+    <input type="text" class="form-control" id="inputAddress" placeholder="Av. 3 de Abril 1200">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Departamento</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="Piso 1">
+  </div>
+  <div class="form-group">
+    <label for="inputAddress2">Timbre</label>
+    <input type="text" class="form-control" id="inputAddress2" placeholder="7b">
+  </div>
+  <div class="form-row">
+    <div class="form-group col-md-6">
+      <label for="inputCity">Celular</label>
+      <input type="text" class="form-control">
+    </div>
+    <div class="form-group col-md-4">
+      <label for="inputState">Empresa</label>
+      <select id="inputState" class="form-control">
+        <option selected>Elegir...</option>
+        <option>Personal</option>
+		<option>Claro</option>
+		<option>Movistar</option>
+		<option>Tuenti</option>
+      </select>
+    </div>
+  </div>
+  <div class="form-group">
+    <div class="form-check">
+      <input class="form-check-input" type="checkbox" id="gridCheck">
+      <label class="form-check-label" for="gridCheck">
+        Recordarme
+      </label>
+    </div>
+  </div>
+  <button type="submit" class="btn btn-success">Registrarse</button>
+  <button type="submit" class="btn btn-danger">Cancelar</button>
+</form>
+
